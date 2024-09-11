9fc33cb384a7783d856b31269203ac80aa81cec1
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-09-11 16:03:50 +0200

    toc working better

--- toc.c
+++ toc.c
@@ -8,7 +8,7 @@ unsigned char *floppy, *floppyend;
 
 enum objecttype { OT_UNKNOWN, OT_MIX, OT_SOUND, OT_SAMPLE };
 struct entry {
-  char name[11];
+  unsigned char data[16];
   int offset;
   enum objecttype type;
 };
@@ -17,6 +17,7 @@ struct entry *toc, *tocend;
 void readfloppy(void) {
   int floppysize = 0, floppycap = 0, tocsize;
   unsigned char *p;
+  struct entry *tt;
 
   while (!feof(stdin)) {
     int n;
@@ -27,55 +28,48 @@ void readfloppy(void) {
 
     if (floppysize == floppycap) {
       floppycap = floppycap ? 2 * floppycap : 1;
-      if (floppy = realloc(floppy, floppycap), !floppy)
+      if (floppy = realloc(floppy, floppycap * sizeof(*floppy)), !floppy)
         err(-1, "realloc floppy");
     }
   }
   floppyend = floppy + floppysize;
 
-  for (p = floppy + 0x200, tocsize = 0; p < floppyend; p += 16, tocsize++) {
+  for (p = floppy + 0x200, tocsize = 0; p < floppyend;
+       p += sizeof(toc->data), tocsize++) {
     struct entry *t;
-    char endoftoc[] = "          \x7f\xff\x00\x00\x00\x00";
-    if (!memcmp(p, endoftoc, sizeof(endoftoc) - 1))
+    if (p[10] == 0x7f)
       break;
 
-    toc = realloc(toc, tocsize + 1);
-    if (!toc)
+    if (toc = realloc(toc, (tocsize + 1) * sizeof(*toc)), !toc)
       err(-1, "realloc toc");
     t = toc + tocsize;
-    memmove(t->name, p, sizeof(t->name) - 1);
-    t->name[sizeof(t->name) - 1] = 0;
-    if (tocsize)
-      t->offset = t[-1].offset + ((int)p[13] << 8) + (int)p[14];
+    memmove(t->data, p, sizeof(t->data));
+    if (t > toc)
+      t->offset = t[-1].offset +
+                  (((int)t[-1].data[13] << 8) + (int)t[-1].data[14]) * 512;
     else
       t->offset = 0x2e00;
-    if (p[11] == 0)
+    if (p[11] == 0x00)
       t->type = OT_MIX;
-    else if (p[11] == 1)
+    else if (p[11] == 0x01)
       t->type = OT_SOUND;
-    else if (p[10] == 63)
+    else if (p[10] == 0x63)
       t->type = OT_SAMPLE;
   }
   tocend = toc + tocsize;
 }
 
 int main(void) {
-  unsigned char *p;
+  struct entry *t;
   int offset = 0;
 
   readfloppy();
-  for (p = floppy + 0x200; p < floppyend; p += 16) {
-    char endoftoc[] = "          \x7f\xff\x00\x00\x00\x00";
-    if (!memcmp(p, endoftoc, sizeof(endoftoc) - 1))
-      break;
-    if (*p & 0x80)
-      printf("s %c", *p ^ 0x80);
-    else
-      printf("  %c", *p);
-    printf("%9.9s", p + 1);
+  for (t = toc; t < tocend; t++) {
+    char *types[] = {"???", "mix", "snd", "sam"};
+    unsigned char *p = t->data;
+    printf("%s %c%9.9s", types[t->type], *p & 0x7f, p + 1);
     printf(", p[10:15]=%02x %02x %02x %02x %02x, offset=%08x", p[10], p[11],
-           p[12], p[13], p[14], 0x2e00 + offset);
-    offset += (((int)p[13] << 8) + (int)p[14]) * 512;
+           p[12], p[13], p[14], t->offset);
     putchar('\n');
 
     if (p[12] || p[15]) {
