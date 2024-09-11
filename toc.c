17c9cb9b0640824aeafba1adf6c6945eb1fad830
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-09-11 16:54:46 +0200

    toc: add comma

--- toc.c
+++ toc.c
@@ -15,7 +15,7 @@ int main(void) {
     char *types[] = {"???", "mix", "snd", "sam"};
     unsigned char *p = t->data;
     printf("%s %c%9.9s", types[t->type], *p & 0x7f, p + 1);
-    printf(", p[10:15]=%02x %02x %02x %02x %02x, offset=%08x len=%d", p[10],
+    printf(", p[10:15]=%02x %02x %02x %02x %02x, offset=%08x, len=%d", p[10],
            p[11], p[12], p[13], p[14], t->offset, t->len);
     putchar('\n');
 
