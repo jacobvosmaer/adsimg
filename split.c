5381592ab7df45aa15a587fa8bf7d2a28765fd85
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-09-11 23:50:17 +0200

    split: operate of floppy sets

--- split.c
+++ split.c
@@ -2,9 +2,9 @@
 #include "adsimg.h"
 #include <err.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-struct floppy floppy;
+#include <unistd.h>
 
 void putle(unsigned x, int n, FILE *f) {
   for (; n > 0; n--, x >>= 8)
@@ -37,33 +37,58 @@ void writewav(unsigned char *data, int n, FILE *f) {
   }
 }
 
-int main(void) {
+struct floppy *floppy;
+int nfloppy;
+
+int main(int argc, char **argv) {
   struct entry *t;
-  int i;
-  char *error = readfloppy(&floppy, stdin);
-  if (error)
-    errx(-1, "read floppy: %s", error);
-  for (t = floppy.toc, i = 0; t < floppy.tocend; t++) {
-    char *p, filename[18];
-    FILE *f;
-    enum { sampleheader = 512, sampletrailer = 512 };
-    if (t->type != OT_SAMPLE)
-      continue;
-    i++;
-    if (snprintf(filename, sizeof(filename), "%02d-%10.10s.wav", i, t->data) !=
-        sizeof(filename) - 1)
-      errx(-1, "filename size error");
-    for (p = filename; *p; p++) {
-      *p &= 0x7f;
-      if (!((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z') ||
-            (*p >= '0' && *p <= '9') || *p == '-' || *p == '.'))
-        *p = '_';
+  struct floppy *fl;
+  int i, j;
+
+  if (argc < 3)
+    errx(-1, "usage: split DIR DISK [DISK...]");
+
+  nfloppy = argc - 2;
+  floppy = calloc(nfloppy, sizeof(*floppy));
+  if (!floppy)
+    errx(-1, "malloc");
+  for (j = 0; j < nfloppy; j++) {
+    char *error, *filename = argv[j + 2];
+    FILE *f = fopen(filename, "rb");
+    if (!f)
+      err(-1, "open %s", filename);
+    error = readfloppy(floppy + j, f);
+    if (error)
+      errx(-1, "readfloppy: %s", error);
+    fclose(f);
+  }
+
+  if (chdir(argv[1]))
+    err(-1, "chdir %s", argv[1]);
+
+  for (fl = floppy, i = 0; fl < floppy + nfloppy; fl++) {
+    for (t = fl->toc; t < fl->tocend; t++) {
+      char *p, filename[18];
+      FILE *f;
+      enum { sampleheader = 512, sampletrailer = 512 };
+      if (t->type != OT_SAMPLE)
+        continue;
+      i++;
+      if (snprintf(filename, sizeof(filename), "%02d-%10.10s.wav", i,
+                   t->data) != sizeof(filename) - 1)
+        errx(-1, "filename size error");
+      for (p = filename; *p; p++) {
+        *p &= 0x7f;
+        if (!((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z') ||
+              (*p >= '0' && *p <= '9') || *p == '-' || *p == '.'))
+          *p = '_';
+      }
+      if (f = fopen(filename, "wb"), !f)
+        err(-1, "fopen %s", filename);
+      writewav(fl->data + t->offset + sampleheader,
+               t->len - sampleheader - sampletrailer, f);
+      if (fclose(f))
+        err(-1, "close %s", filename);
     }
-    if (f = fopen(filename, "wb"), !f)
-      err(-1, "fopen %s", filename);
-    writewav(floppy.data + t->offset + sampleheader,
-             t->len - sampleheader - sampletrailer, f);
-    if (fclose(f))
-      err(-1, "close %s", filename);
   }
 }
